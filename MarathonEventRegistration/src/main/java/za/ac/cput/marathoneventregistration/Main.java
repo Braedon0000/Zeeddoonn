d31548a8ed5fce12f9569341efc55a561603ea43
Braedon0000 <127891987+Braedon0000@users.noreply.github.com>
2023-07-05 11:23:47 +0200

    Update Main.java

--- MarathonEventRegistration/src/main/java/za/ac/cput/marathoneventregistration/Main.java
+++ MarathonEventRegistration/src/main/java/za/ac/cput/marathoneventregistration/Main.java
@@ -4,10 +4,7 @@ package za.ac.cput.marathoneventregistration;
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 import za.ac.cput.marathoneventregistration.gui.MarathonEventGui;
 
-/**
- *
- * @author Leonard
- */
+
 public class Main {
     public static void main(String[] args) 
     {
